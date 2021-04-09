5756413c139a26609f7866c5e9aa036994f1b8eb
Aqeel Aloraibi <aqeel.aloraibi@gmail.com>
2021-04-09 15:20:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Write your code here!
 main.remove();
+<<<<<<< HEAD
 let newHeader =document.createElement("h1");
 newHeader.id="victory";
 newHeader.innerHTML="Aqeel is the champion"
 
 
+=======
+document.createElement("h1");
+>>>>>>> ebee4b9dbf37673c3faec9ef563853c0c4cd4459
